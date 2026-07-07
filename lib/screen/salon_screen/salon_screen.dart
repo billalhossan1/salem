e7b88bc138486cbeac15d8dd2da20fe08f387fa9
Billal Hossan <bh302333@gmail.com>
2026-07-07 12:43:16 +0600

    update according to client logic

--- lib/screen/salon_screen/salon_screen.dart
+++ lib/screen/salon_screen/salon_screen.dart
@@ -1,6 +1,5 @@
 import 'package:core_kit/core_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:zena_app/screen/home_screen/controller/home_screen_controller.dart';
 import 'package:zena_app/widget/notificaiton_widget/notification_widget.dart';
